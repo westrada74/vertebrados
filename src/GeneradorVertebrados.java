3d429a9bb82620fe1c5e44e94a9118f2c86bd80a
westrada74 <westrada@uninorte.edu.co>
2022-09-09 21:01:33 -0500

    version 2: se adicion perro

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -1,10 +1,11 @@
 public class GeneradorVertebrados {
-    public static Vertebrado[] generarArrayVertebrados(){
+    public static Vertebrado[] generarArrayVertebrados() {
         Vertebrado[] miVertebrados = new Vertebrado[4];
-        miVertebrados[0] = new Vertebrado("Camello", "Mamifero"); 
+        miVertebrados[0] = new Vertebrado("Camello", "Mamifero");
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
-        miVertebrados[2] = new Vertebrado("Tiburón", "Pez"); 
-        
+        miVertebrados[2] = new Vertebrado("Tiburón", "Pez");
+        miVertebrados[3] = new Vertebrado("Perro", "Mamifero");
+
         return miVertebrados;
     }
 }
